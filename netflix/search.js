b25244c17846f24396ce59b7a4e85af952b01485
Victor Barros Melo <33198775+vbmelo@users.noreply.github.com>
2021-01-10 21:57:46 +0000

    bugfix, css, details e countries + novo search com filtros

--- netflix/search.js
+++ netflix/search.js
@@ -3,7 +3,12 @@ const inputed = document.getElementById("forminput");
 
 var filtroBusca;
 var baseUri="";
+var baseDetailsUri="";
+var composedUrlDetails="";
 var controleBusca = 0;
+var searchName="";
+var searchId="";
+var searchDetails="";
 
 $( "input" ).on( "click", function() {
     $( "#log" ).html( $( "input:checked" ).val() + " Foi Selecionado Para Busca!" );
@@ -12,24 +17,52 @@ $( "input" ).on( "click", function() {
     switch (filtroBusca) {
         case "filtro_Titulos":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Titles?name=';
+            baseDetailsUri = './titleDetails.html?id=';
+            searchName="Title Name";
+            searchId="Title Id";
+            searchDetails="Title Details";
             break;
         case "filtro_Filmes":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Movies?name=';
+            baseDetailsUri = './moviesDetails.html?id=';
+            searchName="Movie Name";
+            searchId="Movie Id";
+            searchDetails="Movie Details";
             break;
         case "filtro_Series":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Series?name=';
+            baseDetailsUri = './seriesDetails.html?id=';
+            searchName="Series Name";
+            searchId="Series Id";
+            searchDetails="Series Details";
             break;
         case "filtro_Atores":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Actors?name=';
+            baseDetailsUri = './actorDetails.html?id=';
+            searchName="Actor Name";
+            searchId="Actor Id";
+            searchDetails="Actor Details";
             break;
         case "filtro_Categoria":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Categories?name=';
+            baseDetailsUri = './categoriesDetails.html?id=';
+            searchName="Categoria Name";
+            searchId="Category Id";
+            searchDetails="Category Details";
             break;
         case "filtro_Paises":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Countries?name=';
+            baseDetailsUri = './countriesDetails.html?id=';
+            searchName="Country Name";
+            searchId="Country Id";
+            searchDetails="Country Details";
             break;
         case "filtro_Diretores":
             baseUri = 'http://192.168.160.58/netflix/api/Search/Directors?name=';
+            baseDetailsUri = './directorsDetails.html?id=';
+            searchName="Director Name";
+            searchId="Director Id";
+            searchDetails="Director Details";
             break;
     }
     console.log("Base Uri = "+baseUri);
@@ -37,9 +70,13 @@ $( "input" ).on( "click", function() {
 });
 
 $.limparBusca = function(){
+    $("#searchName").empty();
+    $("#searchId").empty();
+    $("#searchDetails").empty();
     $( "#Name" ).empty();
-    $( "#Idd" ).empty();
-    $( "#titleDetails" ).empty();
+    $( "#Id" ).empty();
+    $( "#Details" ).empty();
+    $('#searchError1').hide();
     controleBusca = 0;
     console.log("Busca limpa com sucesso");
 };
@@ -50,13 +87,20 @@ btnsumbit.addEventListener("submit", function(e){
     inputed.value.replace(/\s+/g, '');
     if (baseUri==""){
         baseUri = 'http://192.168.160.58/netflix/api/Search/Titles?name=';
+        baseDetailsUri = './titleDetails.html?id=';
+        searchName="Title Name";
+        searchId="Title Id";
+        searchDetails="Title Details";
     }
     var composedUri = baseUri + inputedSearch;
     console.log("composed Uri = " + composedUri);
     if (controleBusca == 1){
+        $("#searchName").empty();
+        $("#searchId").empty();
+        $("#searchDetails").empty();
         $( "#Name" ).empty();
         $( "#Id" ).empty();
-        $( "#titleDetails" ).empty();
+        $( "#Details" ).empty();
         $('#searchError1').hide();
         controleBusca = 0;
         console.log("Busca limpa com sucesso");
@@ -82,13 +126,16 @@ btnsumbit.addEventListener("submit", function(e){
                     $("#myModalSearch").modal({backdrop: true});
                     $("#myModalSearch").modal("show");
                     $('#searchTable1').show();
+                    $("#searchName").append(searchName);
+                    $("#searchId").append(searchId);
+                    $("#searchDetails").append(searchDetails);
                     $.each(data, function(){
                         $('#Name').append('<li>'+ this['Name']+ '</li>');
                         $('#Id').append('<li>'+ this['Id']+ '</li>');
                         var Id_GET = parseInt(this['Id']);
-                        var baseUrl = './titleDetails.html?id=';
-                        var urlDetails = baseUrl + Id_GET;
-                        $('#titleDetails').append(`<li><a href="${urlDetails}"><i class="fa fa-eye" title="Selecione para ver detalhes"></i></a></li>`);
+                        composedUrlDetails = baseDetailsUri + Id_GET;
+                        console.log('urldetaisl = '+ composedUrlDetails);
+                        $('#Details').append(`<li><a href="${composedUrlDetails}"><i class="fa fa-eye" title="Selecione para ver detalhes"></i></a></li>`);
                     });
                     controleBusca = 1;
                     console.log("Valor do controleBusca = " + controleBusca);

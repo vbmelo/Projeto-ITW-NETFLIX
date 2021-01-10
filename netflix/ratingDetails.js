c48faa6ab63623bf56aee353c27228d7e43d9627
Victor Barros Melo <33198775+vbmelo@users.noreply.github.com>
2021-01-10 22:43:34 +0000

    correcao de bugs e erros dos ratings

---
+++ netflix/ratingDetails.js
@@ -0,0 +1,87 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/netflix/api/Ratings/');
+    self.displayName = 'Ratings Details';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    self.records = ko.observableArray([]);
+    self.indentif = ko.observable('');
+    self.codigo = ko.observable('');
+    self.alvo = ko.observable('');
+    self.desc = ko.observable('');
+    //--- Page Events
+
+  self.activate = function (id) {
+    console.log('CALL: getRating...');
+    var composedUri = self.baseUri() + id;
+    ajaxHelper(composedUri, 'GET').done(function (data) {
+        console.log(data);
+        self.records(data.Titles);
+        self.indentif(data.Id);
+        self.codigo(data.Code);
+        self.alvo(data.Classe);
+        self.desc(data.Description);
+        hideLoading();
+    });
+};
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+
+    }
+    function showLoading() {
+        $('#myModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        });
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    }
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('id');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});

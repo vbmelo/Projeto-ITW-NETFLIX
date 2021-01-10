4d770aef03f2770ec39632b37ce7c0195a1ee7a8
Lucas G. Zanuto <lucasgabrielzanuto@gmail.com>
2021-01-10 19:34:15 +0000

    Ratings + Rating Details
    Ratings + Rating Details.
    Tb arrumei no titles em um lugar que aparece o nome no lugar do ano do filme.

---
+++ netflix/Ratings.js
@@ -0,0 +1,79 @@
+﻿// ViewModel KnockOut
+var vm = function () {
+    console.log('ViewModel initiated...');
+    //---Variáveis locais
+    var self = this;
+    self.baseUri = ko.observable('http://192.168.160.58/netflix/api/Ratings/');
+    self.displayName = 'Ratings';
+    self.error = ko.observable('');
+    self.passingMessage = ko.observable('');
+    self.records = ko.observableArray([]);
+    //--- Page Events
+
+  self.activate = function (id) {
+    console.log('CALL: getActor...');
+    var composedUri = self.baseUri();
+    ajaxHelper(composedUri, 'GET').done(function (data) {
+        console.log(data);
+        self.records(data);
+        hideLoading();
+    });
+};
+    //--- Internal functions
+    function ajaxHelper(uri, method, data) {
+        self.error(''); // Clear error message
+        return $.ajax({
+            type: method,
+            url: uri,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data ? JSON.stringify(data) : null,
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX Call[" + uri + "] Fail...");
+                hideLoading();
+                self.error(errorThrown);
+            }
+        });
+
+    }
+    function showLoading() {
+        $('#myModal').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    }
+    function hideLoading() {
+        $('#myModal').on('shown.bs.modal', function (e) {
+            $("#myModal").modal('hide');
+        });
+    }
+
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    }
+    //--- start ....
+    showLoading();
+    var pg = getUrlParameter('page');
+    console.log(pg);
+    if (pg == undefined)
+        self.activate(1);
+    else {
+        self.activate(pg);
+    }
+};
+
+$(document).ready(function () {
+    console.log("ready!");
+    ko.applyBindings(new vm());
+});
